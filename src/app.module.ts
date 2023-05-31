53f93dc3a21413919fb8f7ad6912e6d1554a267d
Nieopodal <nieopodal.dev@gmail.com>
2023-05-31 14:08:03 +0200

    Installed and configured TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HomeModule } from './home/home.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfiguration } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [HomeModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfiguration,
+    }),
+    HomeModule,
+    NewsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
