816d10b018aecac022826a16ab7c18996cce7023
Nieopodal <nieopodal.dev@gmail.com>
2023-06-02 16:11:08 +0200

    Added the nestjs/throttler, class validator and class transformer.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NewsModule } from './news/news.module';
 import { PhotoModule } from './photo/photo.module';
 import { AdminModule } from './admin/admin.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConfiguration,
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
     }),
     HomeModule,
     NewsModule,
