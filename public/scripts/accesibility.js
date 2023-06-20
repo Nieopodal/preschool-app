870599cd668586b734b5e843c2f0330702c6f093
Nieopodal <nieopodal.dev@gmail.com>
2023-06-20 20:57:09 +0200

    Added the ability to save preferred font size in localstorage.

--- public/scripts/accesibility.js
+++ public/scripts/accesibility.js
@@ -1,36 +1,62 @@
 const zoomInBtn = document.querySelector('#zoom-in-btn');
 const zoomOutBtn = document.querySelector('#zoom-out-btn');
 
-const zoomIn = () => {
-  const textSize = document.getElementsByTagName('html')[0].classList;
-  console.log(textSize[0]);
-  if (textSize[0] === 'text-[150%]') {
-    document.getElementsByTagName('html')[0].classList.remove('text-[150%]');
-    document.getElementsByTagName('html')[0].classList.add('text-[200%]');
+const changeFontSize = (increase) => {
+  const html = document.querySelector('html');
+  const actualTextSize = html.classList;
+
+  if (actualTextSize[0] === 'text-[150%]') {
+    const newValue = increase ? 'text-[200%]' : 'text-[100%]';
+    html.classList.remove('text-[150%]');
+    html.classList.add(newValue);
+    localStorage.setItem('text-size', newValue);
   }
-  if (textSize[0] === 'text-[100%]') {
-    document.getElementsByTagName('html')[0].classList.remove('text-[100%]');
-    document.getElementsByTagName('html')[0].classList.add('text-[150%]');
+  if (increase && actualTextSize[0] === 'text-[100%]') {
+    html.classList.remove('text-[100%]');
+    html.classList.add('text-[150%]');
+    localStorage.setItem('text-size', 'text-[150%]');
+  }
+  if (!increase && actualTextSize[0] === 'text-[200%]') {
+    html.classList.remove('text-[200%]');
+    html.classList.add('text-[150%]');
+    localStorage.setItem('text-size', 'text-[150%]');
   }
 };
 
-const zoomOut = () => {
-  const textSize = document.getElementsByTagName('html')[0].classList;
-  console.log(textSize[0]);
-  if (textSize[0] === 'text-[150%]') {
-    document.getElementsByTagName('html')[0].classList.remove('text-[150%]');
-    document.getElementsByTagName('html')[0].classList.add('text-[100%]');
-  }
-  if (textSize[0] === 'text-[200%]') {
-    document.getElementsByTagName('html')[0].classList.remove('text-[200%]');
-    document.getElementsByTagName('html')[0].classList.add('text-[150%]');
+const getTextSizeFromLocalStorage = () => {
+  const textSizeFromLocalStorage = localStorage.getItem('text-size');
+  const html = document.querySelector('html');
+  const actualTextSize = html.classList[0];
+
+  if (textSizeFromLocalStorage === actualTextSize) return;
+
+  switch (textSizeFromLocalStorage) {
+    case 'text-[100%]': {
+      html.classList.remove(actualTextSize);
+      html.classList.add('text-[100%]');
+      break;
+    }
+    case 'text-[150%]': {
+      html.classList.remove(actualTextSize);
+      html.classList.add('text-[150%]');
+      break;
+    }
+    case 'text-[200%]': {
+      html.classList.remove(actualTextSize);
+      html.classList.add('text-[200%]');
+      break;
+    }
+    default:
+      return;
   }
 };
 
 zoomInBtn.addEventListener('click', () => {
-  zoomIn();
+  changeFontSize(true);
 });
 
 zoomOutBtn.addEventListener('click', () => {
-  zoomOut();
+  changeFontSize(false);
 });
+
+getTextSizeFromLocalStorage();
