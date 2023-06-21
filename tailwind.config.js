0d1c4d8e5f2c68321ff5d025943b5c2777c248b5
Nieopodal <nieopodal.dev@gmail.com>
2023-06-21 12:59:49 +0200

    Added the ability to view the page in high contrast mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     './views/**/*.{html,js,hbs}',
@@ -13,6 +13,33 @@ module.exports = {
     display: ['group-hover'],
   },
   daisyui: {
-    themes: ['cupcake'],
+    themes: [
+      {
+        main: {
+          primary: '#65c3c8',
+          secondary: '#ef9fbc',
+          accent: '#eeaf3a',
+          neutral: '#291334',
+          'base-100': '#faf7f5',
+          info: '#ffffff',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+      },
+      {
+        highContrast: {
+          primary: '#006b70',
+          secondary: '#865064',
+          accent: '#111827',
+          neutral: '#dedede',
+          'base-100': '#f5f5f4',
+          info: '#766060',
+          success: '#24e581',
+          warning: '#d89d13',
+          error: '#fc547e',
+        },
+      },
+    ],
   },
 };
