e88b4eaa7956b0b54e3ec1f8fd7ac5e91de05007
Nieopodal <nieopodal.dev@gmail.com>
2023-07-08 16:39:08 +0200

    Added content structure into gdpr page.

--- src/content/gdpr-page.content.ts
+++ src/content/gdpr-page.content.ts
@@ -1,9 +1,20 @@
 import { Content } from '../types';
 
-export type GdprPageContent = Content;
+export interface GdprPageContent extends Content {
+  sections: {
+    title: string;
+    subtitle?: string;
+    mainListItems: {
+      text: string;
+      paragraphs?: string[];
+      smallListItems?: string[];
+    }[];
+  }[];
+}
 
 export const gdprPageContent: GdprPageContent = {
-  h1: 'Ochrona danych osobowych',
+  h1: 'Polityka prywatności oraz przetwarzanie danych osobowych w mediach społecznościowych',
+  sections: [],
   breadcrumbs: {
     routeUrl: '/ochrona-danych-osobowych',
     routeName: 'Ochrona danych osobowych',
